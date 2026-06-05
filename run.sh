ad8e8a17d474670f51f1ea898098372747d31e42
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-06-05 12:32:48 +0300

    update

--- run.sh
+++ run.sh
@@ -6,13 +6,22 @@ module load julia
 
 # setup
 mkdir -p /tmp/tollande/squashfs_fuse
-squashfuse test.sqfs /tmp/tollande/squashfs_fuse
+squashfuse_ll test.sqfs /tmp/tollande/squashfs_fuse
 unsquashfs -dest /tmp/tollande/squashfs_tmp test.sqfs > /dev/null
 unsquashfs -dest ./squashfs_lustre test.sqfs > /dev/null
 
 # benchmark
+echo "/tmp fresh"
 julia read_files.jl /tmp/tollande/squashfs_tmp 10000
+echo "/tmp cached"
+julia read_files.jl /tmp/tollande/squashfs_tmp 10000
+echo "squashfuse_ll fresh"
 julia read_files.jl /tmp/tollande/squashfs_fuse 10000
+echo "squashfuse_ll cached"
+julia read_files.jl /tmp/tollande/squashfs_fuse 10000
+echo "lustre fresh"
+julia read_files.jl ./squashfs_lustre 10000
+echo "lustre cached"
 julia read_files.jl ./squashfs_lustre 10000
 
 # cleanup
