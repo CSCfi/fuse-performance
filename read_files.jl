8b06ef62162fb7e5f864e6237bf7bd4ed9a370a0
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-06-08 12:35:37 +0300

    read files

--- read_files.jl
+++ read_files.jl
@@ -10,10 +10,28 @@ function read_files(dir::AbstractString, order::Vector{Int})
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
-    length(ARGS) == 2 || error("usage: julia read_files.jl <directory> <N>")
-    dir = ARGS[1]
-    N = parse(Int, ARGS[2])
+    length(ARGS) == 1 || error("usage: julia read_files.jl <N>")
+    N = parse(Int, ARGS[1])
     order = shuffle!(collect(1:N))
-    read_files("non-existent", Int[])  # precompile function
-    @time read_files(dir, order)
+
+    # precompile function
+    read_files("non-existent", Int[])
+
+    println("/tmp fresh")
+    @time read_files("/tmp/tollande/squashfs_tmp", order)
+
+    println("/tmp cached")
+    @time read_files("/tmp/tollande/squashfs_tmp", order)
+
+    println("squashfuse_ll fresh")
+    @time read_files("/tmp/tollande/squashfs_fuse", order)
+
+    println("squashfuse_ll cached")
+    @time read_files("/tmp/tollande/squashfs_fuse", order)
+
+    println("lustre scratch fresh")
+    @time read_files("/scratch/project_2001659/squashfs_lustre", order)
+
+    println("lustre scratch cached")
+    @time read_files("/scratch/project_2001659/squashfs_lustre", order)
 end
--- run_read.sh
+++ run_read.sh
@@ -11,27 +11,11 @@ module load julia
 mkdir -p /tmp/tollande/squashfs_fuse
 squashfuse_ll nocomp.sqfs /tmp/tollande/squashfs_fuse
 
-# TODO: also time unsquashfs time
 unsquashfs -dest /tmp/tollande/squashfs_tmp nocomp.sqfs > /dev/null
 unsquashfs -dest /scratch/project_2001659/squashfs_lustre nocomp.sqfs > /dev/null
 
 # benchmark
-# TODO: run many tests with same file access order
-
-echo "/tmp fresh"
-julia read_files.jl /tmp/tollande/squashfs_tmp 10000
-echo "/tmp cached"
-julia read_files.jl /tmp/tollande/squashfs_tmp 10000
-
-echo "squashfuse_ll fresh"
-julia read_files.jl /tmp/tollande/squashfs_fuse 10000
-echo "squashfuse_ll cached"
-julia read_files.jl /tmp/tollande/squashfs_fuse 10000
-
-echo "lustre scratch fresh"
-julia read_files.jl /scratch/project_2001659/squashfs_lustre 10000
-echo "lustre scratch cached"
-julia read_files.jl /scratch/project_2001659/squashfs_lustre 10000
+julia read_files.jl 10000
 
 # cleanup
 fusermount -u /tmp/tollande/squashfs_fuse
